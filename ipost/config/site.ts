31af08f3a8dfe47a3281660b2f1ff7f227f4f966
kuldeepsharma1 <hytek112@gmail.com>
2024-05-31 21:57:38 +0530

    intial version 0.1.0 installation and setup

---
+++ ipost/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "IPOST",
+  url: "https://example.com",
+  description: "Welcome to ipost that post that your",
+  author: "Kuldeep Sharma",
+  links: {
+    twitter: "https://twitter.com/kuldeepit1",
+    github: "https://github.com/kuldeepsharma1",
+    personalSite: "https://hytek.org.in",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
